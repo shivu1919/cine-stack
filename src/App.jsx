0bc9f9a332c5255085e601e2e66f23283d780c18
shivu1919 <shivamthakur.ss129@gmail.com>
2025-04-12 15:08:20 +0530

    Added watchlist logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,75 @@ import './App.css'
 
 function App() {
 
-  const[num, setNum] = useState(0)
+  const[count, setCount] = useState(0)
+  const[movies, setMovies] = useState([
+    {
+    name:"The Sky Force",
+    image:'/the-sky-force.png',
+    watchlist:'/add.png'
+    },
+    {
+      name:"SuperBoys of Malegaon",
+      image:'/superboys.png',
+      watchlist:'/add.png'
+    },
+    {
+      name:"All That Breathes",
+      image:"/all-that-breathes.png",
+      watchlist:'/add.png'
+    },
+    {
+      name:"Pink",
+      image:"/pink.png",
+      watchlist:'/add.png'
+    },
+    {
+      name:"Queen",
+      image:'/queen.png',
+      watchlist:'/add.png'
+    },
+    {
+      name:"Berlin",
+      image:"/berlin.png",
+      watchlist:'/add.png'
+    },
+    {
+      name:"Crazxy",
+      image:"/crazxy.png",
+      watchlist:'/add.png'
+    },
+    {
+      name:"Girls will be Girls",
+      image:"/girls.png",
+      watchlist:'/add.png'
+    },
+    {
+      name:"Udta Punjab",
+      image:"/punjab.png",
+      watchlist:'/add.png'
+    },
+    {
+      name:"3 Idiots",
+      image:"/3idiots.png",
+      watchlist:'/add.png'
+    }
+])
+
+  const addToWatchlist = (index) =>{
+
+    if(movies[index].watchlist=='/remove.png'){
+      setCount(count-1)
+      movies[index].watchlist='/add.png'
+      setMovies([...movies])
+    }
+    else{
+      setCount(count+1)
+      movies[index].watchlist='/remove.png'
+      setMovies([...movies])
+    }
+
+      
+  }
 
   return (
    <>
@@ -11,116 +79,27 @@ function App() {
         <div className='first'>
             <img src="/Capture.PNG" width="100"/>
             
-            <button id='watchlist-btn'>Watchlist {num}</button>
+            <button id='watchlist-btn'>Watchlist : {count}</button>
         </div>
         <br/>
-        <hr/>
-
+       
+        <br/><br/>
         <br/>
         <div className='second'>
-              <div id='movie'>
-                <img src="/the-sky-force.png" height="200" />
-                
-                <div id="watchlist">
-                  <h2>The Sky Force</h2>
-                  <img src="/add.png" />
-                </div>
-                
-                
-              </div>
-              <div id='movie'>
-                <img src="/the-sky-force.png" height="200" />
-                
-                <div id="watchlist">
-                  <h2>The Sky Force</h2>
-                  <img src="/add.png" />
-                </div>
-                
-                
-              </div>
-              <div id='movie'>
-                <img src="/the-sky-force.png" height="200" />
-                
-                <div id="watchlist">
-                  <h2>The Sky Force</h2>
-                  <img src="/add.png" />
-                </div>
-                
-                
-              </div>
-              <div id='movie'>
-                <img src="/the-sky-force.png" height="200" />
-                
-                <div id="watchlist">
-                  <h2>The Sky Force</h2>
-                  <img src="/add.png" />
-                </div>
-                
-                
-              </div>
-              <div id='movie'>
-                <img src="/the-sky-force.png" height="200" />
-                
-                <div id="watchlist">
-                  <h2>The Sky Force</h2>
-                  <img src="/add.png" />
-                </div>
-                
-                
-              </div>
-        </div>
-      <br></br>
-        <div className='third'>
-        <div id='movie'>
-                <img src="/the-sky-force.png" height="200" />
-                
-                <div id="watchlist">
-                  <h2>The Sky Force</h2>
-                  <img src="/add.png" />
-                </div>
-                
-                
-              </div>
-              <div id='movie'>
-                <img src="/the-sky-force.png" height="200" />
-                
-                <div id="watchlist">
-                  <h2>The Sky Force</h2>
-                  <img src="/add.png" />
-                </div>
-                
-                
-              </div>
-              <div id='movie'>
-                <img src="/the-sky-force.png" height="200" />
-                
-                <div id="watchlist">
-                  <h2>The Sky Force</h2>
-                  <img src="/add.png" />
-                </div>
-                
-                
-              </div>
-              <div id='movie'>
-                <img src="/the-sky-force.png" height="200" />
-                
-                <div id="watchlist">
-                  <h2>The Sky Force</h2>
-                  <img src="/add.png" />
-                </div>
-                
-                
-              </div>
-              <div id='movie'>
-                <img src="/the-sky-force.png" height="200" />
-                
-                <div id="watchlist">
-                  <h2>The Sky Force</h2>
-                  <img src="/add.png" />
-                </div>
-                
-                
-              </div>
+
+          {movies.map((item,index)=>
+            <div id='movie'>
+            <img src={item.image} height="200" />
+            <div id="watchlist">
+              <p id="para">{item.name}</p>
+              <img 
+              id="watchlist-image" 
+              src={item.watchlist} 
+              width="25" 
+              onClick={()=>{addToWatchlist(index)}}/>
+            </div>
+          </div>
+          )}
         </div>
     </div>
    </>
